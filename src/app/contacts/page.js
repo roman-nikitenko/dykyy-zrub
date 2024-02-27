cdafcda6d2b7e85c77d201833511494f934952ce
Roman Nikitenko <Lomkaaa@ukr.net>
2024-02-27 11:03:08 +0200

    Added burger menu on mobile version

--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
-    <div className='bg-amber-200'>
-      <h1>Contacts page</h1>
+    <div className='xl:px-18 px-5 lg:px-12 2xl:px-40'>
+      <h1 className='decorationLine my-16'>Контакти</h1>
     </div>
   );
 };
