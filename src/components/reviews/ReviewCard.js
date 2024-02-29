e8287eefb04308e2ece3515970d3128c1846c922
Adriana S <adrianasuhanova@gmail.com>
2024-02-29 20:25:33 +0200

    add modal window

--- src/components/reviews/ReviewCard.js
+++ src/components/reviews/ReviewCard.js
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import Image from 'next/image';
+import GalleryModal from '@/components/reviews/GalleryModal';
+
+const imagesSrc = ['/images/house1.jpeg', '/images/house2.jpeg', '/images/interior1.jpeg'];
 
 const ReviewCard = ({ review }) => {
+  const [isModalShown, setIsModalShown] = useState(false);
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+
+  const handleImageClick = (index) => {
+    setIsModalShown(true);
+    setActiveImageIndex(index);
+  };
+
   return (
     <div className='grid h-[380px] place-content-between rounded-2xl p-[25px] shadow-md md:shadow-lg '>
       <div>
@@ -10,25 +22,23 @@ const ReviewCard = ({ review }) => {
         </p>
       </div>
       <div className='flex place-content-center gap-[5px]'>
-        {/*{review.images.map(imageSrc => (*/}
-        {/*  <Image*/}
-        {/*    src={imageSrc}*/}
-        {/*    alt='house image'*/}
-        {/*    width={100}*/}
-        {/*    height={100}*/}
-        {/*    className=''*/}
-        {/*  />*/}
-        {/*))}*/}
-        <Image src={'/images/house1.jpeg'} alt='house image' width={95} height={95} className='' />
-        <Image src={'/images/house2.jpeg'} alt='house image' width={95} height={95} className='' />
-        <Image
-          src={'/images/interior1.jpeg'}
-          alt='house image'
-          width={95}
-          height={95}
-          className=''
-        />
+        {imagesSrc.map((imageSrc, index) => (
+          <Image
+            key={imageSrc}
+            src={imageSrc}
+            alt='house image'
+            width={100}
+            height={100}
+            onClick={() => handleImageClick(index)}
+          />
+        ))}
       </div>
+      <GalleryModal
+        isModalShown={isModalShown}
+        activeImageIndex={activeImageIndex}
+        imagesSrc={imagesSrc}
+        setIsModalShown={setIsModalShown}
+      />
     </div>
   );
 };
