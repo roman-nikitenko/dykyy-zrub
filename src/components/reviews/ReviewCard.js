14250146ff1f41d8a5bcbd1da376430be34985de
Adriana S <adrianasuhanova@gmail.com>
2024-02-29 13:59:00 +0200

    update styles of swiper

--- src/components/reviews/ReviewCard.js
+++ src/components/reviews/ReviewCard.js
@@ -2,9 +2,13 @@ import Image from 'next/image';
 
 const ReviewCard = ({ review }) => {
   return (
-    <div className='grid h-[380px] max-h-[390px] max-w-[380px] rounded-2xl p-[25px] shadow-md sm:shadow-xl xl:max-w-[430px]'>
-      <h2 className='mb-[10px] text-Green-light'>{review.name}</h2>
-      <p className='mb-[35px] font-thin italic leading-tight text-Green-700'>{review.text}</p>
+    <div className='grid h-[380px] place-content-between rounded-2xl p-[25px] shadow-md md:shadow-lg '>
+      <div>
+        <h2 className='pb-[5px] text-Green-light xl:text-[24px]'>{review.name}</h2>
+        <p className=' font-thin italic leading-tight text-Green-700 xl:text-[15px]'>
+          {review.text}
+        </p>
+      </div>
       <div className='flex place-content-center gap-[5px]'>
         {/*{review.images.map(imageSrc => (*/}
         {/*  <Image*/}
@@ -15,25 +19,13 @@ const ReviewCard = ({ review }) => {
         {/*    className=''*/}
         {/*  />*/}
         {/*))}*/}
-        <Image
-          src={'/images/house1.jpeg'}
-          alt='house image'
-          width={100}
-          height={100}
-          className=''
-        />
-        <Image
-          src={'/images/house2.jpeg'}
-          alt='house image'
-          width={100}
-          height={100}
-          className=''
-        />
+        <Image src={'/images/house1.jpeg'} alt='house image' width={95} height={95} className='' />
+        <Image src={'/images/house2.jpeg'} alt='house image' width={95} height={95} className='' />
         <Image
           src={'/images/interior1.jpeg'}
           alt='house image'
-          width={100}
-          height={100}
+          width={95}
+          height={95}
           className=''
         />
       </div>
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -13,27 +13,26 @@ const breakpoints = {
   },
   768: {
     slidesPerView: 2,
-    spaceBetween: 35,
+    spaceBetween: 20,
   },
   1024: {
     slidesPerView: 3,
-    spaceBetween: 25,
+    spaceBetween: 20,
   },
   1440: {
     slidesPerView: 3,
-    spaceBetween: 60,
+    spaceBetween: 40,
   },
 };
 
 const Reviews = () => {
   return (
-    <section>
-      <h2 className='decorationLine ml-[25px] text-[30px] font-bold text-Green-700 sm:mb-[20px] lg:mb-[25px] lg:text-[35px] xl:mb-[40px] xl:text-[42px]'>
+    <section className='m-auto max-w-[1440px]'>
+      <h2 className='decorationLine mb-[5px] text-[30px] font-bold text-Green-700 sm:mb-[20px] lg:mb-[25px] lg:text-[35px] xl:mb-[40px] xl:text-[42px]'>
         Відгуки наших клієнтів
       </h2>
       <div className='flex justify-center'>
         <Swiper
-          spaceBetween={50}
           slidesPerView={1}
           breakpoints={breakpoints}
           modules={[Autoplay, Navigation, Keyboard, Pagination]}
@@ -41,11 +40,11 @@ const Reviews = () => {
           pagination={{ clickable: true }}
           keyboard={{ enabled: true }}
           autoplay={{
-            delay: 3500,
+            delay: 5500,
             disableOnInteraction: false,
           }}
           loop={true}
-          className='w-[380px] sm:w-[640px] md:w-[768px] lg:w-[1024px] xl:w-[1440px]'
+          className='w-[380px] sm:w-[640px] md:w-[750px] lg:w-[1000px] xl:w-[1300px]'
         >
           {reviewsList.map((review) => (
             <SwiperSlide key={review.id}>
