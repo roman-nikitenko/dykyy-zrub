df898a37c71f94dbf13e815be18f99c340e8a735
Pavlo Voloshyn <pavlo.voloshyn@promova.com>
2024-03-17 15:21:44 +0200

    feat: add text into Logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -3,10 +3,13 @@ import Link from 'next/link';
 
 const Logo = () => {
   return (
-    <Link href='/'>
+    <Link href='/' className={'no-wrap flex items-center'}>
       <div className='relative size-10 cursor-pointer'>
         <Image src='/images/logo2.png' fill sizes='100%' alt='logo' />
       </div>
+      <span className={'text-nowrap pl-3 text-base font-bold  text-white lg:text-2xl'}>
+        Дикий зруб
+      </span>
     </Link>
   );
 };
