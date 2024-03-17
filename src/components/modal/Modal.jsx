c66c5009e623b01f28957886f33ee4cd4f3a5cb4
Pavlo Voloshyn <pavlo.voloshyn@promova.com>
2024-03-17 15:21:22 +0200

    feat: add 'ask us' form into Modal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,6 +2,7 @@
 import classNames from 'classnames';
 
 import { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
 
 const Frame = ({
   children,
@@ -26,40 +27,43 @@ const Frame = ({
 
   return (
     // transparent overlay: `inset-0` to stretch over the entire screen (combines`top-0`, `right-0`, `bottom-0`, and `left-0`)
-    <div className='flex w-full flex-col items-center justify-center'>
-      {/* overlay */}
-      <div
-        className={classNames(
-          'fixed inset-0 z-10 bg-gray-600/80 p-8 text-white',
-          `${open ? 'visible' : 'invisible'}`
-        )}
-        onClick={closeOnClickOutside ? onOverlayClick : undefined}
-      >
-        <div className='relative mx-auto mt-8 w-full max-w-sm' ref={container}>
-          <button
-            className='absolute -right-2 -top-2 flex h-8 w-8 cursor-pointer justify-center rounded-full bg-gray-600 shadow-xl'
-            onClick={onClose}
-            title='Bye bye'
-          >
-            <span className='select-none text-2xl leading-7'>&times;</span>
-          </button>
-          <div className='overflow-hidden rounded bg-gray-800 shadow-xl'>{children}</div>
-        </div>
+
+    <div
+      className={classNames(
+        'fixed inset-0 z-10 bg-gray-600/80 p-8 text-white',
+        `${open ? 'visible' : 'invisible'}`
+      )}
+      onClick={closeOnClickOutside ? onOverlayClick : undefined}
+    >
+      <div className='relative top-2/4 mx-auto w-full max-w-sm -translate-y-2/4' ref={container}>
+        <button
+          className='absolute -right-2 -top-2 flex h-8 w-8 cursor-pointer justify-center rounded-full bg-gray-600 shadow-xl'
+          onClick={onClose}
+          title='Bye bye'
+        >
+          <span className='select-none text-2xl leading-7'>&times;</span>
+        </button>
+        <div className='overflow-hidden rounded bg-gray-800 shadow-xl'>{children}</div>
       </div>
     </div>
   );
 };
 
-export const Modal = ({ title, children }) => {
+export const Modal = ({ buttonTitle, title, children }) => {
   const [open, setOpen] = useState(false);
+  const defaultClassNames =
+    'rounded-[8px] border border-Yellow-500 bg-Yellow-500 px-[15px] py-[6px] text-[12px] font-bold tracking-wide text-white hover:border-Brown-500 hover:bg-Brown-500 lg:px-[24px] lg:py-[8px] lg:text-[15px] xl:px-[35px] xl:py-[11px] xl:text-[18px]';
+  const btnClassnames =
+    'justify-self-center flex no-wrap justify-center leading-8 text-nowrap rounded-[8px]  bg-Yellow-500 px-3 py-1 font-bold tracking-wide text-white transition hover:bg-yellow-800 md:block xl:text-12 max-h-full';
 
+  // TODO: separate form from Modal component
   return (
     <>
-      <button
-        className='rounded-[8px] border border-Yellow-500 bg-Yellow-500 px-[15px] py-[6px] text-[12px] font-bold tracking-wide text-white hover:border-Brown-500 hover:bg-Brown-500 lg:px-[24px] lg:py-[8px] lg:text-[15px] xl:px-[35px] xl:py-[11px] xl:text-[18px]'
-        onClick={() => setOpen(true)}
-      >
-        Open the modal
+      <button className={btnClassnames || defaultClassNames} onClick={() => setOpen(true)}>
+        <span className='hidden xl:inline-flex'>{buttonTitle || 'Задати питання'}</span>
+        <div className='relative size-6 justify-center xl:inline-flex'>
+          <Image src='/icons/message.svg' alt='ask question icon' fill />
+        </div>
       </button>
       <Frame
         open={open}
@@ -67,9 +71,61 @@ export const Modal = ({ title, children }) => {
           setOpen(false);
         }}
       >
-        <div className='block bg-green-700 p-4'>
+        <div className=' max-w-sm  rounded-lg p-4 shadow-md'>
           <h1 className='text-lg'>{title}</h1>
+          <form className={'align-center mx-auto flex flex-col'}>
+            <div className='mb-4'>
+              <label htmlFor='name' className='block text-sm font-medium text-yellow-50'>
+                Ваше імя
+              </label>
+              <input
+                type='text'
+                id='name'
+                className='form-control block w-full rounded-md border border-gray-300 px-3 py-2 text-black focus:outline-none focus:ring-1 focus:ring-indigo-500'
+                placeholder='Іван'
+              />
+            </div>
+            <div className='mb-4'>
+              <label htmlFor='phone' className='block text-sm font-medium text-yellow-50'>
+                Телефон
+              </label>
+              <input
+                type='tel'
+                id='phone'
+                className='form-control block w-full rounded-md border border-gray-300 px-3 py-2 text-black focus:outline-none focus:ring-1 focus:ring-indigo-500'
+                placeholder='0671234567'
+              />
+            </div>
+            <div className='mb-4'>
+              <label htmlFor='email' className='block text-sm font-medium text-yellow-50'>
+                Ваш Email
+              </label>
+              <input
+                type='email'
+                id='email'
+                className=' form-control block w-full rounded-md border border-gray-300 px-3 py-2 text-black focus:outline-none focus:ring-1 focus:ring-indigo-500'
+                placeholder='myemail@example.com'
+              />
+            </div>
+            <div className='mb-4'>
+              <label htmlFor='message' className='block text-sm font-medium text-yellow-50'>
+                Питання
+              </label>
+              <textarea
+                id='message'
+                className='form-control block h-32 w-full rounded-md border border-gray-300 px-3 py-2 text-black focus:outline-none focus:ring-1 focus:ring-indigo-500'
+                placeholder='Напишіть ваше запитання тут'
+              />
+            </div>
+            <button
+              type='submit'
+              className='inline-flex items-center justify-center rounded-md bg-yellow-800 px-4 py-2 font-bold text-white hover:bg-Yellow-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+            >
+              Відправити питання
+            </button>
+          </form>
         </div>
+
         {/* <div className="p-4"> */}
         {/*   <div className="flex flex-col space-y-2"> */}
         {/*     <input className="text-gray-800 outline-none border-2 border-white focus:border-blue-300 p-1" */}
