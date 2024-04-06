087b995f9c06b9bb00c1f3ef251608dabc0022a9
Roman Nikitenko <Lomkaaa@ukr.net>
2024-04-06 17:00:54 +0300

    Change gallery

--- src/components/gallery/MainGallery.jsx
+++ src/components/gallery/MainGallery.jsx
@@ -2,9 +2,6 @@
 import { useState } from 'react';
 import GalleryModal from '@/components/galleryModal/GalleryModal';
 import { galleryImages } from '@/data/galleryImages';
-import PhotoAlbum from 'react-photo-gallery';
-import NextJsImage from '@/components/gallery/NextJsImage';
-import photos from '@/components/gallery/photos';
 
 const MainGallery = () => {
   const [isModalShown, setIsModalShown] = useState(false);
@@ -17,17 +14,21 @@ const MainGallery = () => {
 
   return (
     <>
-      <PhotoAlbum
-        layout='rows'
-        photos={photos}
-        renderPhoto={NextJsImage}
-        defaultContainerWidth={1200}
-        sizes={{ size: '100vw' }}
-        onClick={(_, photos) => {
-          handleImageClick(photos.photo);
-        }}
-      />
-
+      <div className='columns-1 gap-x-2 md:columns-2 lg:columns-3 '>
+        {galleryImages.map((photo, index) => (
+          <div
+            key={photo.imageLink + index}
+            onClick={() => handleImageClick(photo.src)}
+            className='mb-2 w-full cursor-pointer transition duration-[500] ease-in hover:scale-[1.02]'
+          >
+            <img
+              src={photo.imageLink}
+              alt={`Gallary emage ${photo.imageLink}`}
+              className='max-w-full rounded-md'
+            />
+          </div>
+        ))}
+      </div>
       <GalleryModal
         navigationArrowsShown
         isModalShown={isModalShown}
@@ -37,22 +38,6 @@ const MainGallery = () => {
         setIsModalShown={setIsModalShown}
       />
     </>
-
-    // <div className='flex flex-wrap cursor-pointer grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 '>
-    //
-    //   {/*{arrayImages.map((image, index) => (*/}
-    //   {/*  <div className='relative w-[200px] h-[200px] border  rounded-lg' key={index}>*/}
-    //   {/*    <Image*/}
-    //   {/*      src={image.imageLink}*/}
-    //   {/*      className='rounded-lg object-cover transition duration-300 hover:scale-[1.03]'*/}
-    //   {/*      alt='gallery-image'*/}
-    //   {/*      fill*/}
-    //   {/*      onClick={() => handleImageClick(image.imageLink)}*/}
-    //   {/*    />*/}
-    //   {/*  </div>*/}
-    //   {/*))}*/}
-    //
-    // </div>
   );
 };
 
