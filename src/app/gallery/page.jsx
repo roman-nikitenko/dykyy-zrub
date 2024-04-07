33b395b58c3a5989a5ecbb484aa0a27f628af445
roman-nikitenko <89584222+roman-nikitenko@users.noreply.github.com>
2024-04-07 12:09:59 +0300

    R: Masonry gallery (#23)
    * added Masonry gallery
    
    * Change gallery
    
    * Fixed bug with click on photo
    
    * chore: replace img on Image component; refactor NextJsImage component
    
    * chore: increase main banner height
    
    ---------
    
    Co-authored-by: Pavlo Voloshyn <pavlo.voloshyn@promova.com>

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -1,5 +1,4 @@
 import MainGallery from '@/components/gallery/MainGallery';
-import { galleryImages } from '@/data/galleryImages';
 
 export const metadata = {
   title: 'Галерея',
@@ -10,7 +9,7 @@ const Gallery = () => {
     <section className='flex justify-center'>
       <div className='container-spaces max-width-1440 pb-[100px] lg:pb-[160px]'>
         <h1 className='decorationLine my-12 md:my-[64px]'>Галерея</h1>
-        <MainGallery arrayImages={galleryImages} />
+        <MainGallery />
       </div>
     </section>
   );
