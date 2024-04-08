b4fc33204f7141020bf6daa97b32c832e27ef851
Roman Nikitenko <Lomkaaa@ukr.net>
2024-04-08 20:21:01 +0300

    added description stages building

--- src/components/constructionStages/ConstructionStages.jsx
+++ src/components/constructionStages/ConstructionStages.jsx
@@ -4,49 +4,49 @@ const ConstructionStages = () => {
   const listOfStages = [
     {
       id: 1,
-      title: 'Project Planning',
+      title: 'Планування та дизайн',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto deleniti dolore est illum iusto, laboriosam provident quasi quod sed sunt.',
+        'Цей початковий крок включає концептуалізацію будівлі, створення архітектурних проектів, отримання необхідних дозволів і забезпечення відповідності місцевим нормам. Архітектори, інженери та дизайнери співпрацюють, щоб окреслити структуру, функціональність та естетичні аспекти споруди.',
       imageUrl: '/images/advantageImages/planning.svg',
       styles: '',
     },
     {
       id: 2,
-      title: 'Project Management',
+      title: 'Підготовка майданчика',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto deleniti dolore est illum iusto, laboriosam provident quasi quod sed sunt.',
+        'Після завершення розробки проекту та отримання дозволів починається етап підготовки ділянки. Це передбачає розчищення будівельної території, планування землі для створення рівної поверхні та встановлення тимчасових об’єктів, таких як огорожа та інженерні комунікації.',
       imageUrl: '/images/advantageImages/management.svg',
       styles: 'md:translate-y-[60px]',
     },
     {
       id: 3,
-      title: 'General Contracting',
+      title: 'Будівництво фундаменту',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto deleniti dolore est illum iusto, laboriosam provident quasi quod sed sunt.',
+        'Роботи з фундаменту є критично важливими, оскільки вони забезпечують структурну опору та стабільність будівлі. Залежно від таких факторів, як умови ґрунту та вимоги до будівництва, можуть бути споруджені різні типи фундаментів, як-от плита на рівні, простір для повзання або підвал.',
       imageUrl: '/images/advantageImages/building.svg',
       styles: 'md:translate-y-[120px]',
     },
     {
       id: 4,
-      title: 'Interior Design',
+      title: 'Будівництво',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto deleniti dolore est illum iusto, laboriosam provident quasi quod sed sunt.',
+        'Після створення фундаменту починається власне будівництво будівлі. На цьому етапі зводиться каркас конструкції, включаючи стіни, перекриття і дах. Кваліфіковані робітники, такі як теслі, муляри та електрики, працюють разом, щоб зібрати будівельні компоненти відповідно до архітектурних планів.',
       imageUrl: '/images/advantageImages/design.svg',
       styles: '',
     },
     {
       id: 5,
-      title: 'Exterior Design',
+      title: 'Внутрішнє оздоблення',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto deleniti dolore est illum iusto, laboriosam provident quasi quod sed sunt.',
+        'Після завершення обшивки будівлі починаються внутрішні оздоблювальні роботи. Це включає в себе встановлення ізоляції, гіпсокартону, підлоги, сантехніки, електропроводки та систем ОВКВ. Для покращення естетики та функціональності внутрішніх приміщень також можуть бути залучені дизайнери інтер’єру.',
       imageUrl: '/images/advantageImages/design-tool.svg',
       styles: 'md:translate-y-[60px]',
     },
     {
       id: 6,
-      title: 'Space Planning',
+      title: 'Зовнішня обробка та ландшафтний дизайн',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto deleniti dolore est illum iusto, laboriosam provident quasi quod sed sunt.',
+        'Останній крок передбачає додавання зовнішньої обробки, такої як сайдинг, фарба, вікна та двері, щоб завершити зовнішній вигляд будівлі. Елементи ландшафтного дизайну, такі як тротуари, під’їзні шляхи, зелені насадження та зовнішні зручності, також встановлюються, щоб покращити оточення та створити затишне середовище.',
       imageUrl: '/images/advantageImages/planning2.svg',
       styles: 'md:translate-y-[120px]',
     },
